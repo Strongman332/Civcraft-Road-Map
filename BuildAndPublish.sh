7537f5bd8683650bd06f28d86b7aac1f9cc3a1f8
Strongman332 <Strongman332@gmail.com>
2013-05-22 00:39:06 -0300

    switched from xalan to xsltproc.

--- BuildAndPublish.sh
+++ BuildAndPublish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#GenerateAndPublish.sh
+#BuildAndPublish.sh
 #
 #use to publish map to gh-pages (http://strongman332.github.io/Civcraft-Road-Map/)
-#use GenerateTestMap.sh for testing
+#use BuildTestMap.sh for testing
 #
-#depends- xalan, git
+#depends- xsltproc, git
 #
 
 TODAY=$(date +%D)
@@ -16,7 +16,7 @@ rm -r -f gh-pages
 echo Clone gh-pages from github
 git clone -b gh-pages https://github.com/Strongman332/Civcraft-Road-Map.git gh-pages
 echo Generating map.svg
-xalan -in map.xml -xsl map.xslt -out gh-pages/map.svg
+xsltproc -novalid -o gh-pages/map.svg map.xslt map.xml
 cd gh-pages
 git add map.svg
 git commit -a -m "Auto Build $TODAY;$HOUR" && ( echo Please log in && ((git push origin gh-pages||git push origin gh-pages) && echo SUCCESS!) ||  echo FAILED!) ||  echo UPTODATE!
